9138930ea9f25fc5ee84861730cd5b93138d0ead
LibreKosei <cryptokell@tutanota.com>
2025-05-30 20:40:47 +0800

    Enabled ntp service

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@
 
   # Set your time zone.
     time.timeZone = "Asia/Kuala_Lumpur";
+    services.ntp.enable = true;
     networking.timeServers = options.networking.timeServers.default ++ ["ntp.example.com"];
 
     fonts = {
