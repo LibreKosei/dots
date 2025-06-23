62612b98bfc44e9e7b1358323e91de3dc854823d
LibreKosei <cryptokell@tutanota.com>
2025-06-23 23:52:29 +0800

    services.automatic-timezoned.enable = true

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,8 @@
         wget
         python3
     ];
+
+    services.automatic-timezoned.enable = true;
   
     system.stateVersion = "24.11"; # Did you read the comment?
     nix.settings.experimental-features = ["nix-command" "flakes"];
