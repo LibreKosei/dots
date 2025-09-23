b608805d030b7cc181fd8b3b348b5cd11ea1fbfd
LibreKosei <cryptokell@tutanota.com>
2025-09-23 11:38:38 +0900

    feat: wireshark group

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.kosei = {
         isNormalUser = true;
-        extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+        extraGroups = [ "wheel" "libvirtd" "wireshark" ]; # Enable ‘sudo’ for the user.
         packages = with pkgs; [
             tree
         ];
