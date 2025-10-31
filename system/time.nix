7c59e92040c1143f82ef115d22395d02f09de50f
LibreKosei <cryptokell@tutanota.com>
2025-10-31 21:04:00 +0800

    feat: time.hardwareClockInLocalTime = true;

--- system/time.nix
+++ system/time.nix
@@ -1,5 +1,6 @@
 {config, pkgs, ...}:
 {
-    time.timeZone = "Asia/Kuala_Lumpur";
-    services.timesyncd.enable = true;
+    # time.timeZone = "Asia/Kuala_Lumpur";
+    # services.timesyncd.enable = true;
+    time.hardwareClockInLocalTime = true;
 }
