42167beabf949e505c04a37eb66dec795838135e
LibreKosei <cryptokell@tutanota.com>
2025-10-31 21:06:30 +0800

    removal: virtualization.libvirtd.qemu.ovmf

--- system/virt-manager/default.nix
+++ system/virt-manager/default.nix
@@ -8,13 +8,13 @@
             package = pkgs.qemu_kvm;
             runAsRoot = true;
             swtpm.enable = true;
-            ovmf = {
-                enable = true;
-                packages = [(pkgs.OVMF.override {
-                    secureBoot = true;
-                    tpmSupport = true;
-                }).fd];
-            };
+            # ovmf = {
+            #     enable = true;
+            #     packages = [(pkgs.OVMF.override {
+            #         secureBoot = true;
+            #         tpmSupport = true;
+            #     }).fd];
+            # };
         };
     };
     virtualisation.spiceUSBRedirection.enable = true;
