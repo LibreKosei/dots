52b87ae25f54ac0128c86433c65c52683d9c979e
LibreKosei <cryptokell@tutanota.com>
2025-11-19 19:45:49 +0800

    feat: KDE Connect

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,18 @@
 
     # time 
     time.timeZone = "Asia/Kuala_Lumpur";
+
+    # KDE Connect
+    programs.kdeconnect.enable = true;
+    networking.firewall = {
+        enable = true;
+        allowedTCPPortRanges = [
+            { from = 1714; to = 1764; } # KDE Connect
+        ];
+        allowedUDPPortRanges = [
+            { from = 1714; to = 1764; } # KDE Connect
+        ];
+    };
 }
 
 
