04e5a1914efbe4b088a2d656915c3e2024c59e93
LibreKosei <cryptokell@tutanota.com>
2026-02-27 01:03:45 +0800

    feat: declare modules as a freeform submodule tree

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,13 @@
-{config, ...}:
+{lib, ...}:
 {
+    options.modules = lib.mkOption {
+        type = lib.types.submodule {
+            freeformType = lib.types.attrsOf lib.types.anything;
+        };
+        default = {};
+        description = "Custom system module namespace";
+    };
+
     imports = [
         ./core 
         ./desktop 
