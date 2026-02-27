0e4b3534709e84437446eb9cf53c19cf7c8cb20d
LibreKosei <cryptokell@tutanota.com>
2026-02-27 16:26:12 +0800

    feat: better submodule definition

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,9 +1,26 @@
 {lib, ...}:
 {
     options.modules = lib.mkOption {
-        type = lib.types.submodule {
-            freeformType = lib.types.attrsOf lib.types.anything;
-        };
+        type = lib.types.submodule ({lib, ...}: {
+            options = {
+                core = lib.mkOption {
+                    type = lib.types.submodule {};
+                    default = {};
+                };
+                desktop = lib.mkOption {
+                    type = lib.types.submodule {};
+                    default = {};
+                };
+                utils = lib.mkOption {
+                    type = lib.types.submodule {};
+                    default = {};
+                };
+                games = lib.mkOption {
+                    type = lib.types.submodule {};
+                    default = {};
+                };
+            };
+        });
         default = {};
         description = "Custom system module namespace";
     };
